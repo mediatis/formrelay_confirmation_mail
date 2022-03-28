6178f17cd422eac8cff09fa69a4a539dbe50fd98
Meelis Karulin <karulin@mediatis.de>
2022-03-28 21:29:11 +0200

    Use the php package

--- Classes/Initialization.php
+++ Classes/Initialization.php
@@ -3,21 +3,12 @@
 namespace Mediatis\FormrelayConfirmationMail;
 
 use FormRelay\Core\Service\RegistryInterface;
-use Mediatis\FormrelayConfirmationMail\DataDispatcher\ConfirmationMailDataDispatcher;
-use Mediatis\FormrelayConfirmationMail\Route\ConfirmationMailRoute;
-use Mediatis\FormrelayMail\Manager\MailManager;
+use FormRelay\ConfirmationMail\ConfirmationMailInitialization;
 
 class Initialization
 {
-    /**
-     * @param RegistryInterface $registry
-     * @return void
-     */
     public function initialize(RegistryInterface $registry)
     {
-        $registry->registerDataDispatcher(ConfirmationMailDataDispatcher::class, [
-            new MailManager(),
-        ]);
-        $registry->registerRoute(ConfirmationMailRoute::class);
+        ConfirmationMailInitialization::initialize($registry);
     }
 }
